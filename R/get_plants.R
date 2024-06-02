7dda5903d7a71b522f97b0346c20b3bb90f2574d
David Smith <david.alex.smith@gmail.com>
2024-06-02 20:04:51 +0100

    Get plant details

---
+++ R/get_plants.R
@@ -0,0 +1,25 @@
+
+#' Get details of power plants associated with a Sunsynk account
+#'
+#' @param token 
+#'
+#' @return List.
+#' @export
+#'
+#' @examples
+get_plants <- function(token) {
+  
+  url <- "https://api.sunsynk.net/api/v1/plants?page=1&limit=100&name=&status="
+  
+  req <- httr2::request(url) |> 
+    httr2::req_headers(authorization = paste0("Bearer ", token$data$access_token))
+  
+  res <- req |> 
+    httr2::req_perform()
+  
+  res <- res |> 
+    httr2::resp_body_json()
+  
+  res
+  
+}
