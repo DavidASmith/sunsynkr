bf4dbfe73095fa188f1b3e2c0ccebdf06e6c2225
David Smith <david.alex.smith@gmail.com>
2024-07-31 21:19:29 +0100

    Default to local timezone for timestamps

--- R/day_summary_info_to_col.R
+++ R/day_summary_info_to_col.R
@@ -2,6 +2,10 @@
 #'
 #' @param x Daily summary
 #' @param date Date to be pre-pended to the time to form a date/time field. 
+#' @param tz Timezone for the output time. Note that time returned by the API 
+#' is in local time (including DST). It's likely that you'll want this set to 
+#' your local timezone (the default). However, you can specify any timezone as 
+#' required (use `OlsonNames()` to return all valid timezones for your system). 
 #'
 #' @return data.frame
 #' @export
@@ -12,7 +16,7 @@
 #' }
 #' @importFrom rlang .data
 #' @importFrom rlang := 
-day_summary_info_to_col <- function(x, date) {
+day_summary_info_to_col <- function(x, date, tz = Sys.timezone()) {
   
   label <- x$label
   unit <- x$unit
@@ -24,7 +28,7 @@ day_summary_info_to_col <- function(x, date) {
     lapply(function(x) tibble::tibble(dt = x$time, value = x$value)) |>
     dplyr::bind_rows() |>
     dplyr::mutate(dt = paste0(date, " ", .data$dt)) |>
-    dplyr::mutate(dt = lubridate::ymd_hm(.data$dt)) |>
+    dplyr::mutate(dt = lubridate::ymd_hm(.data$dt, tz = tz)) |>
     dplyr::mutate(value = as.numeric(.data$value)) |> 
     dplyr::rename(!!rlang::as_name(col_name) := .data$value)
   
--- R/get_day_summary_table.R
+++ R/get_day_summary_table.R
@@ -3,6 +3,10 @@
 #' @param token Authorisation token
 #' @param plant_id plant id
 #' @param date Date for the summary (YYYY-MM-DD).
+#' @param tz Timezone for the output time. Note that time returned by the API 
+#' is in local time (including DST). It's likely that you'll want this set to 
+#' your local timezone (the default). However, you can specify any timezone as 
+#' required (use `OlsonNames()` to return all valid timezones for your system). 
 #'
 #' @return A data.frame
 #' @export
@@ -11,7 +15,7 @@
 #' \dontrun{
 #' get_day_summary_table(token, 1, "2024-07-30")
 #' }
-get_day_summary_table <- function(token, plant_id, date) {
+get_day_summary_table <- function(token, plant_id, date, tz = Sys.timezone()) {
   
   day_summary <- get_day_summary(token, 
                                  plant_id, 
@@ -22,7 +26,8 @@ get_day_summary_table <- function(token, plant_id, date) {
   
   out <- infos |> 
     lapply(day_summary_info_to_col, 
-           date = date) |> 
+           date = date, 
+           tz = tz) |> 
     purrr::reduce(dplyr::left_join, by = 'dt') |> 
     dplyr::arrange(.data$dt)
   
